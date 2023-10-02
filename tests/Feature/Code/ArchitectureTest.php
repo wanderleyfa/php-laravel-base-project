a582529c635e67497a998e79dac956c69eea24cb
wanderley-ferreira <wanderley.albuquerque@ibrowse-sds.com.br>
2023-10-02 17:16:33 -0300

    test: implementing architectural tests

---
+++ tests/Feature/Code/ArchitectureTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Model;
+
+test('debugging functions are not used')
+    ->expect(['dd', 'dump', 'print_r', 'ray', 'var_dump'])
+    ->not->toBeUsed();
+
+test('models')
+    ->expect('App\Models')
+    ->classes()
+    ->toExtend(Model::class);
+
+test('concerns are traits')
+    ->expect('App\Models\Concerns')
+    ->toBeTraits();
+
+test('controllers have the "Controller" suffix')
+    ->expect('App\Http\Controllers')
+    ->toHaveSuffix('Controller');
+
+test('controllers extend the base controller')
+    ->expect('App\Http\Controllers')
+    ->classes()
+    ->toExtend(Controller::class);
+
+test('tests have the "Test" suffix')
+    ->expect('Tests\Feature')
+    ->toHaveSuffix('Test');
--- tests/Feature/Code/DebugFuntionsTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-test('there are debug functions')
-    ->expect(['dd', 'ddd', 'dump', 'var_dump', 'print_r'])
-    ->not->toBeUsed();
